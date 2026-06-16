99fa6385ef9bd07080459e6ab0a8735c0d2d942c
Abdelrahman Samy <abdelrahmansamy127@gmail.com>
2026-06-16 23:28:50 +0300

    Initial Commit

---
+++ lib/core/theming/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const kPrimaryColor = Color(0xff247CFF);
+  static const kGray = Color(0xff757575);
+}
---
+++ lib/doc_app.dart
@@ -0,0 +1,28 @@
+import 'package:doctor/core/routing/app_router.dart';
+import 'package:doctor/core/routing/routes.dart';
+import 'package:doctor/core/theming/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.kPrimaryColor,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
