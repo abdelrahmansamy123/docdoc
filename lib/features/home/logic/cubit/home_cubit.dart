e853aa69bee2fc229b0eeef5dc33e768947f05e7
Abdelrahman Samy <abdelrahmansamy127@gmail.com>
2026-09-16 21:22:51 +0300

    - Filter doctor by ID - Shimmer laoding - UI enhancements for speciality image

--- lib/features/home/logic/cubit/home_cubit.dart
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -1,4 +1,7 @@
+import 'package:doctor/core/helper/extention.dart';
+import 'package:doctor/core/networking/api_error_handler.dart';
 import 'package:doctor/core/networking/api_result.dart';
+import 'package:doctor/features/home/data/models/specializations_response_model.dart';
 import 'package:doctor/features/home/data/repo/home_repo.dart';
 import 'package:doctor/features/home/logic/cubit/home_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,16 +10,47 @@ class HomeCubit extends Cubit<HomeState> {
   final HomeRepo _homeRepo;
   HomeCubit(this._homeRepo) : super(HomeState.initial());
 
+  List<SpecializationsData?>? specializationsList = [];
+
   void getSpecializations() async {
     emit(const HomeState.specializationsLoading());
     final response = await _homeRepo.getSpecialization();
     response.when(
       success: (specializationsResponseModel) {
-        emit(HomeState.specializationsSuccess(specializationsResponseModel));
+        specializationsList =
+            specializationsResponseModel.specializationDataList ?? [];
+
+        // getting the doctors list for the first specialization by default.
+        getDoctorsList(specializationId: specializationsList?.first?.id);
+
+        emit(
+          HomeState.specializationsSuccess(
+            specializationsResponseModel.specializationDataList,
+          ),
+        );
       },
       failure: (errorHandler) {
         emit(HomeState.specializationsError(errorHandler));
       },
     );
+  }
+
+  void getDoctorsList({required int? specializationId}) {
+    List<Doctors?>? doctorsList = getDoctorsListBySpecializationId(
+      specializationId,
+    );
+
+    if (!doctorsList.isNullOrEmpty()) {
+      emit(HomeState.doctorsSuccess(doctorsList));
+    } else {
+      emit(HomeState.doctorsError(ErrorHandler.handle('No doctors found')));
+    }
+  }
+
+  /// returns the list of doctors based on the specialization id
+  getDoctorsListBySpecializationId(specializationId) {
+    return specializationsList
+        ?.firstWhere((specialization) => specialization?.id == specializationId)
+        ?.doctorsList;
   }
 }
--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:doctor/core/helper/spacing.dart';
 import 'package:doctor/features/home/ui/widgets/doctor_blue_container.dart';
 import 'package:doctor/features/home/ui/widgets/doctor_specialty_see_all.dart';
+import 'package:doctor/features/home/ui/widgets/doctors_list/doctors_bloc_builder.dart';
 import 'package:doctor/features/home/ui/widgets/home_top_bar.dart';
-import 'package:doctor/features/home/ui/widgets/specializations_and_doctors_bloc_builder.dart';
+import 'package:doctor/features/home/ui/widgets/specializations_list/specializations_bloc_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,10 +22,12 @@ class HomeScreen extends StatelessWidget {
             children: [
               const HomeTopBar(),
               const DoctorBlueContainer(),
-              verticalSpace(24.h),
+              verticalSpace(24),
               const DoctorSpecialtySeeAll(),
-              verticalSpace(18.h),
-              const SpecializationsAndDoctorsBlocBuilder(),
+              verticalSpace(18),
+              const SpecializationsBlocBuilder(),
+              verticalSpace(8),
+              const DoctorsBlocBuilder(),
             ],
           ),
         ),
